eb8c9f013b61064768012438416b139cab6c473d
SonDN <sondoannam202@gmail.com>
2025-01-31 19:19:55 +0700

    feat(app): :rocket: setup theme

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    "prettier",
+    "plugin:tailwindcss/recommended"
+  ),
 ];
 
 export default eslintConfig;
